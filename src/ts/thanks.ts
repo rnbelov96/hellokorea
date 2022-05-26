51e325bfcc55fa15690d65970c0ced230a00f400
Belov Roman <rnbelov96@gmail.com>
2022-05-26 17:53:21 +0300

    Initiate the project

---
+++ src/ts/thanks.ts
@@ -0,0 +1,12 @@
+import '../scss/thanks.scss';
+import './year';
+// import './modals';
+
+const userName = localStorage.getItem('userName');
+
+const nameLabelEl = document.querySelector('.js-name') as HTMLSpanElement;
+nameLabelEl.textContent = userName
+  ? `${localStorage.getItem('userName')}, б`
+  : 'Б';
+
+document.title = userName ? `${userName}, спасибо, Ваша заявка принята` : 'Cпасибо, Ваша заявка принята';
